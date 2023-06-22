1c8ea427d4aa005cd001f657f8922c3a85ed7859
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-06-22 21:03:17 +0200

    add

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 export const Spinner = ({ text = '', size = '5em' }) => {
-  const header = text ? <h4>{text}</h4> : null
-  return (
+	const header = text ? <h4>{text}</h4> : null
+	return (
     <div className="spinner">
       {header}
       <div className="loader" style={{ height: size, width: size }} />
     </div>
+	  <div className="loader" style={{ height: size, width: size }} />
+	  </div > //это не надо
   )
 }
